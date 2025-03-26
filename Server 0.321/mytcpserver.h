4bb9204408d58ac7bd99bee25ae31536f0ee2d39
Ruslan Starkov <r.v.starkov@stud.mospolytech.ru>
2025-03-26 17:30:34 +0300

    Server 0.321
    Added Database

---
+++ Server 0.321/mytcpserver.h
@@ -0,0 +1,27 @@
+#ifndef MYTCPSERVER_H
+#define MYTCPSERVER_H
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QtNetwork>
+#include <QByteArray>
+#include <QDebug>
+#include <QMap>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = nullptr);
+    ~MyTcpServer();
+public slots:
+    void slotNewConnection();
+    void slotClientDisconnected();
+    void slotServerRead();
+private:
+    QTcpServer * mTcpServer;
+    QTcpSocket * mTcpSocket;
+    int server_status;
+    QMap<int, QTcpSocket*> mSocketDescriptors; // Хранение дескрипторов сокетов
+};
+#endif  // MYTCPSERVER_H
