a6001f7468ad8901233bb9597e6c47177dd5408e
Ruslan Starkov <GraffixBoy420@yandex.ru>
2025-04-26 15:56:16 +0300

    Server 0.332
    -Исправлены ошибки связанные с дублированием записей в таблице Users
    -Переработана функция reg()
    -Исправлена проверка уникальности email

---
+++ Server 0.332/func2serv.h
@@ -0,0 +1,26 @@
+#ifndef FUNC2SERV_H
+#define FUNC2SERV_H
+
+#include <QByteArray>
+
+QByteArray parsing(QString input, int socdes);
+
+QByteArray auth(QStringList );
+QByteArray reg(QStringList);
+QByteArray get_stat(/*QStringList*/);
+QByteArray check_task(/*QStringList*/);
+QByteArray menu_export(/*QStringList*/);
+QByteArray get_products(QStringList params);
+QByteArray get_all_users();
+QByteArray get_stable_stat();
+int get_user_count();
+int get_product_count();
+QByteArray get_dynamic_stat();
+int get_weekly_logins();
+int get_monthly_logins();
+QByteArray add_favorite_ration(const QStringList& container);
+bool add_ration_to_favorites(const QString& userId, const QString& rationId);
+
+#include <QDebug>
+
+#endif // FUNC2SERV_H
