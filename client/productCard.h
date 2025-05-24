1a718631dd84cc7d6adf59e1a87c22d39d633fba
Egor Deev <67710823+IGlek@users.noreply.github.com>
2025-05-24 16:15:08 +0300

    Delete client directory

--- client/productCard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QWidget>
-
-
-class productCard : public QWidget {
-    Q_OBJECT
-public:
-    explicit productCard(QString name, int price, int proteins, int fatness, int carbs, QWidget *parent = nullptr);
-};
